0155241e8d8bdacd2da598c61d31476ee2c87201
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-01 13:26:47 -0300

    atts

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -16,9 +16,12 @@ namespace EstacionamentoEmCsharp.Models
         }
         public void adicionarVeiculo()
         {
+            Console.WriteLine("E N T R A D A   D E   V E Í C U L O S");
+            Thread.Sleep(1000);
             Console.Clear();
             Console.WriteLine("Digite a placa do veiculo");
             var placa = (Console.ReadLine());
+            Thread.Sleep(2000);
             Console.Clear();
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
@@ -26,11 +29,41 @@ namespace EstacionamentoEmCsharp.Models
                 Thread.Sleep(2000);
                 Console.Clear();
             }
+            else
+            {
+                veiculos.Add(placa.Trim());
+                Console.WriteLine("Veículo cadastrado com sucesso!\n");
+            }
 
         }
         public void removerVeiculo()
         {
+            int horas = 0;
+            Console.Clear();
+            Console.WriteLine("S A Í D A  D E  V E Í C U L O S");
+            Thread.Sleep(1000);
+            Console.Clear();
 
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            var placa = Console.ReadLine();
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veiculo ficou:");
+                Thread.Sleep(1000);
+                Console.Clear();
+                horas = int.Parse(Console.ReadLine());
+                decimal valorTotal = 0;
+
+                valorTotal = (precoInicial + precoHora) * horas;
+
+                veiculos.Remove(placa.Trim());
+                Thread.Sleep(1000);
+                Console.Clear();
+                Thread.Sleep(1000);
+                Console.WriteLine($"O Veiculo {placa} foi removido e ficou no valor : {valorTotal}");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
         }
         public void listarVeiculo()
         {
